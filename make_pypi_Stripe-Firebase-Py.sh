46adf5067635c897701a2caab07135e95735e8e2
ruben <ruben@moonshot.codes>
2023-08-17 15:23:34 -0400

    changes

--- make_pypi_Stripe-Firebase-Py.sh
+++ make_pypi_Stripe-Firebase-Py.sh
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="Stripe-Firebase-Py",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     py_modules=["stripe_models"],
     long_description=long_description,
